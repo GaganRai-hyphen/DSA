5e24940139d7227ac154311fb61ca46ee89b5407
Gagan Rai <gaganrai2246@gmail.com>
2026-04-15 15:42:00 +0530

    Create 2365-PercentageOfLetterInString

---
+++ 2365-PercentageOfLetterInString/2365-PercentageOfLetterInString.java
@@ -0,0 +1,14 @@
+// Last updated: 4/15/2026, 3:41:29 PM
+class Solution {
+    public int percentageLetter(String s, char letter) {
+     int c = 0;
+     for(int i = 0 ; i<s.length() ; i++){
+       char ch = s.charAt(i);
+      
+        if(ch == letter){
+             c++;
+             }
+     }   
+     return (c*100/s.length()) ;
+    }
+}
