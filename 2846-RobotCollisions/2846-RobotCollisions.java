37fb8f23e3ef46970ef3fd795bea62240a647d97
Gagan Rai <gaganrai2246@gmail.com>
2026-04-15 15:41:43 +0530

    Create 2846-RobotCollisions

---
+++ 2846-RobotCollisions/2846-RobotCollisions.java
@@ -0,0 +1,44 @@
+// Last updated: 4/15/2026, 3:41:12 PM
+class Solution {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+        
+        Arrays.sort(indices, (a, b) -> Integer.compare(positions[a], positions[b]));
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i : indices) {
+            if (directions.charAt(i) == 'R') {
+                stack.push(i);
+            } else {
+                while (!stack.isEmpty() && healths[i] > 0) {
+                    int top = stack.peek();
+                    if (healths[top] < healths[i]) {
+                        healths[i]--;
+                        healths[top] = 0;
+                        stack.pop();
+                    } else if (healths[top] > healths[i]) {
+                        healths[top]--;
+                        healths[i] = 0;
+                    } else {
+                        healths[i] = 0;
+                        healths[top] = 0;
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (healths[i] > 0) {
+                result.add(healths[i]);
+            }
+        }
+        return result;
+    }
+}
