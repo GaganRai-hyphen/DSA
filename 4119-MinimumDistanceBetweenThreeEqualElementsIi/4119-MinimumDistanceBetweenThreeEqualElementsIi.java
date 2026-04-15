e9a33f576313055a33ba2f952fa55e22d446ff78
Gagan Rai <gaganrai2246@gmail.com>
2026-04-15 15:40:40 +0530

    Create 4119-MinimumDistanceBetweenThreeEqualElementsIi

---
+++ 4119-MinimumDistanceBetweenThreeEqualElementsIi/4119-MinimumDistanceBetweenThreeEqualElementsIi.java
@@ -0,0 +1,25 @@
+// Last updated: 4/15/2026, 3:40:09 PM
+class Solution {
+    public int minimumDistance(int[] nums) {
+        int[] lastSeen = new int[100005];
+        int[] secondLastSeen = new int[100005];
+        
+        Arrays.fill(lastSeen, -1);
+        Arrays.fill(secondLastSeen, -1);
+        
+        int minDistance = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            
+            if (secondLastSeen[val] != -1) {
+                minDistance = Math.min(minDistance, 2 * (i - secondLastSeen[val]));
+            }
+            
+            secondLastSeen[val] = lastSeen[val];
+            lastSeen[val] = i;
+        }
+        
+        return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
+    }
+}
