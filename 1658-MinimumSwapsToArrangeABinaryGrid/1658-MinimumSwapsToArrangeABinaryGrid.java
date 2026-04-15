c43b40f7cd4096b3db9abe74683e908f3611084e
Gagan Rai <gaganrai2246@gmail.com>
2026-04-15 15:42:58 +0530

    Create 1658-MinimumSwapsToArrangeABinaryGrid

---
+++ 1658-MinimumSwapsToArrangeABinaryGrid/1658-MinimumSwapsToArrangeABinaryGrid.java
@@ -0,0 +1,44 @@
+// Last updated: 4/15/2026, 3:42:27 PM
+class Solution {
+    public int minSwaps(int[][] grid) {
+        int n = grid.length;
+        int[] trailingZeros = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = n - 1; j >= 0; j--) {
+                if (grid[i][j] == 0) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            trailingZeros[i] = count;
+        }
+        
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int target = n - 1 - i;
+            int foundIdx = -1;
+            
+            for (int j = i; j < n; j++) {
+                if (trailingZeros[j] >= target) {
+                    foundIdx = j;
+                    break;
+                }
+            }
+            
+            if (foundIdx == -1) return -1;
+            
+            for (int j = foundIdx; j > i; j--) {
+                int temp = trailingZeros[j];
+                trailingZeros[j] = trailingZeros[j - 1];
+                trailingZeros[j - 1] = temp;
+                res++;
+            }
+        }
+        
+        return res;
+        
+    }
+}
