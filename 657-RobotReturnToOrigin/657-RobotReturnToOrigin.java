e627497977232b8b3112f695ecfa3959cf3b35a1
Gagan Rai <gaganrai2246@gmail.com>
2026-04-15 15:44:09 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,22 @@
+// Last updated: 4/15/2026, 3:43:39 PM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        
+        for (char move : moves.toCharArray()) {
+            if (move == 'U') {
+                y++;
+            } else if (move == 'D') {
+                y--;
+            } else if (move == 'L') {
+                x--;
+            } else if (move == 'R') {
+                x++;
+            }
+        }
+        
+        return x == 0 && y == 0;
+        
+    }
+}
