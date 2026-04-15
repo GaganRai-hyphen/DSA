2575aaca087b86c11cac3a1bbb784cfdee79f0dc
Gagan Rai <gaganrai2246@gmail.com>
2026-04-15 15:42:56 +0530

    Create 1667-FindKthBitInNthBinaryString

---
+++ 1667-FindKthBitInNthBinaryString/1667-FindKthBitInNthBinaryString.java
@@ -0,0 +1,20 @@
+// Last updated: 4/15/2026, 3:42:26 PM
+class Solution {
+    public char findKthBit(int n, int k) {
+        if (n == 1) {
+            return '0';
+        }
+        
+        int length = (1 << n) - 1;
+        int mid = length / 2 + 1;
+        
+        if (k == mid) {
+            return '1';
+        } else if (k < mid) {
+            return findKthBit(n - 1, k);
+        } else {
+            char bit = findKthBit(n - 1, length - k + 1);
+            return bit == '0' ? '1' : '0';
+        }
+    }
+}
